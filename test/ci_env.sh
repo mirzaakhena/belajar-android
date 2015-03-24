fd01becbaabc81a142b78fbb8434fa5b67fe204a
Roy Clarkson <rclarkson@pivotal.io>
2015-03-24 17:20:47 -0500

    Update CI script

--- test/ci_env.sh
+++ test/ci_env.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd $(dirname $0)
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    wget http://dl.google.com/android/android-sdk_r22.6.2-linux.tgz -nv
-    tar xvf android-sdk_r22.6.2-linux.tgz > /dev/null
+    wget http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz -nv
+    tar xvf android-sdk_r24.1.2-linux.tgz > /dev/null
     export ANDROID_HOME=`pwd`/android-sdk-linux 
     export PATH=${ANDROID_HOME}/tools/:${ANDROID_HOME}/platform-tools/:${PATH}
-    echo "y" | android update sdk -f -u -a -t tools,platform-tools,build-tools-19.0.3,android-19,extra-android-m2repository,extra-android-support
+    echo "y" | android update sdk -f -u -a -t tools,platform-tools,build-tools-22.0.1,android-22,extra-android-m2repository,extra-android-support
 #elif [[ "$OSTYPE" == "darwin"* ]]; then
 else
     echo "OS is not linux"
